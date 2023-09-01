1d2539fc3ee3f0e8ccadfa757fd2db9ca0bed1e4
skrewde <94148640+skrewde@users.noreply.github.com>
2023-09-01 02:15:06 +0100

    Switched major files
    Created a construction page

---
+++ scripts.js
@@ -0,0 +1,14 @@
+        // Array of background colors to cycle through
+        const colors = ["#006ab1", "#4a0521", "#5d2965", "#d6cfe2", "#5966f1", "#003c64", "#297e55", "#eeada9"];
+
+        // Function to set a random background color
+        function setRandomBackgroundColor() {
+            const randomIndex = Math.floor(Math.random() * colors.length);
+            document.body.style.backgroundColor = colors[randomIndex];
+        }
+
+        // Set a random background color when the page loads
+        window.addEventListener("load", setRandomBackgroundColor);
+
+        // You can also add an event listener to change the color on a button click or other event
+        // Example: document.getElementById("changeColorButton").addEventListener("click", setRandomBackgroundColor);
