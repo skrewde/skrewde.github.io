93681d40508d0e00120323f923a21057925d8827
skrewde <94148640+skrewde@users.noreply.github.com>
2023-09-01 21:38:03 +0100

    Bugfixes
    Fixed HTML

--- scripts.js
+++ scripts.js
@@ -1,5 +1,5 @@
         // Array of background colors to cycle through
-        const colors = ["#006ab1", "#4a0521", "#5d2965", "#d6cfe2", "#5966f1", "#003c64", "#297e55", "#eeada9"];
+        const colors = ["#006ab1", "#4a0521", "#5d2965", "#3f3d3b", "#333a8b", "#003c64", "#297e55", "#c9918e", "#b78f19"];
 
         // Function to set a random background color
         function setRandomBackgroundColor() {
